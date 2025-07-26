dc179b44a60e2908edfc407473604ea0773b69a3
Rajat Gupta <test@Rajats-MacBook-Pro.local>
2025-07-26 21:28:51 +0530

    edit hybrid

--- components/filter-tabs.tsx
+++ components/filter-tabs.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 export default function FilterTabs() {
   const filters = [
     "SUVs under $200k",
-    "Used Sedans with Hybrid",
+    "Hybrid",
     "Electric Cars",
     "Sports Cars",
     "Family Vans",
