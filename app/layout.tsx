782663ae092b9bf1ae4169824005bfc9f4be4d75
forrune <esoifeoma1@gmail.com>
2025-08-25 10:25:47 +0100

    Update Layout Styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <meta name="robots" content="index, follow" />
         </head>
         <body
-          className={`${soraFont.variable} ${spaceGroteskFont.variable} antialiased`}
+          className={`${soraFont.variable} ${spaceGroteskFont.variable} antialiased overflow-hidden`}
         >
           <LayoutWrapper>{children}</LayoutWrapper>
         </body>
