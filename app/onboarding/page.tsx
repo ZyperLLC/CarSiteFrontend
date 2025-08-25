5b31da139fa1a8a4e79f8b754ee72d378e48323e
forrune <esoifeoma1@gmail.com>
2025-08-25 10:34:12 +0100

    Update Onboarding

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -5,7 +5,7 @@ export default async function OnboardingPage() {
   const sellers = await getSellers()
 
   return (
-    <section className="min-h-screen flex items-center justify-center pb-24 pt-32 sm:pt-40">
+    <section className=" flex items-center justify-center pb-24 pt-32 sm:pt-40">
       <div className="w-full max-w-2xl px-4">
         <OnboardingForm />
       </div>
